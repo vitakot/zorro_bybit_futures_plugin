a322f291b18832d320b467023361ebaae80a6425
Vitezslav Kot <vitezslav.kot@stonky.cz>
2026-04-25 00:56:05 +0200

    Update copyright to Stonky s.r.o.

--- include/bybit_futures.h
+++ include/bybit_futures.h
@@ -3,7 +3,7 @@ Bybit Futures Zorro Plugin
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 SPDX-License-Identifier: MIT
-Copyright (c) 2025 Vitezslav Kot <vitezslav.kot@gmail.com>.
+Copyright (c) 2025 Vitezslav Kot <vitezslav.kot@stonky.cz>, Stonky s.r.o.
 */
 
 #ifndef BYBIT_FUTURES_ZORRO_PLUGIN_H
